a427a0610d88e4fb80617cb3154603450c4adcb4
tiger-lion-2 <337169083@qq.com>
2022-08-02 16:52:02 +0800

    ...

--- 字符串替换.py
+++
@@ -1,8 +0,0 @@
-# 使用字符串的replace方法可以把字符串中指定子串替换成其他内容。语法格式：
-# str.replace(old,new[,max])
-
-str = 'cat dog cat dog cat dog'
-str1 = str.replace('cat', 'mouse')
-str2 = str.replace('cat', 'mouse', 2)
-print("str1:", str1)
-print("str2:", str2)
