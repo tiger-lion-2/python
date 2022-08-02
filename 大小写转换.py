2a6125693f14729f875122da728261608d15de80
tiger-lion-2 <337169083@qq.com>
2022-08-02 15:22:06 +0800

    字符串

---
+++ 大小写转换.py
@@ -0,0 +1,11 @@
+# str.capitalize(),将字符串中第一个字母大写，其他字母都小写
+# str.lower(),将字符串中所有字母都小写
+# str.upper(),将字符串中所有字母都大写
+# str.swapcase(),将字符串中小写字母变大写，大写字母变小写
+
+str = 'i Like Python'
+print("原字符串：", str)
+print('capitalize方法的结果：', str.capitalize())
+print('lower方法的结果：', str.lower())
+print('upper方法的结果：', str.upper())
+print('swapcase方法的结果：', str.swapcase())
